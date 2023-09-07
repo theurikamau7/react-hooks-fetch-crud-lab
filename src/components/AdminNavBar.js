b1ff86588bac7cedeb697ec963bd3ae39d0db8db
IanKamau <theurikamau7@gmail.com>
2023-09-07 19:15:44 +0300

    completed assignment

--- src/components/AdminNavBar.js
+++ src/components/AdminNavBar.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-function AdminNavBar({ onChangePage }) {
+function AdminNavBar({ PageChange }) {
   return (
     <nav>
-      <button onClick={() => onChangePage("Form")}>New Question</button>
-      <button onClick={() => onChangePage("List")}>View Questions</button>
+      <button onClick={() => PageChange("Form")}>New Question</button>
+      <button onClick={() => PageChange("List")}>View Questions</button>
     </nav>
   );
 }
--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,12 @@ import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
-  const [page, setPage] = useState("List");
+  const [currentPage, setCurrentPage] = useState("List");
 
   return (
     <main>
-      <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      <AdminNavBar PageChange={setCurrentPage} />
+      {currentPage === "Form" ? <QuestionForm /> : <QuestionList />}
     </main>
   );
 }
